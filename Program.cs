735e6588dd25ce8f62dace02d5240a73cbd6fdb0
Daniel <daniil.nyashka@gmail.com>
2024-03-02 19:55:43 +0300

    done

--- Program.cs
+++ Program.cs
@@ -4,76 +4,70 @@ class Program
 {
     static void Main()
     {
-         string[] TreeVasya =Console.ReadLine().Split();
-        string[] TreeMasha = Console.ReadLine().Split();
+        string[] firstMatch = Console.ReadLine().Split(':');
+        int firstMatch1 = Convert.ToInt32(firstMatch[0]);
+        int firstMatch2 = Convert.ToInt32(firstMatch[1]);
 
-        int bucketVasya = Convert.ToInt32(TreeVasya[0]);
-        int lengthVasya = Convert.ToInt32(TreeVasya[1]) * 2;
-        int startVasya = bucketVasya - lengthVasya / 2;
-        int endVasya = bucketVasya + lengthVasya / 2;
+        string[] currentMatch = Console.ReadLine().Split(':');
+        int currentMatch1 = Convert.ToInt32(currentMatch[0]);
+        int currentMatch2 = Convert.ToInt32(currentMatch[1]);
 
-        int bucketMasha = Convert.ToInt32(TreeMasha[0]);
-        int lengthMasha = Convert.ToInt32(TreeMasha[1]) * 2; 
-        int startMasha = bucketMasha - lengthMasha / 2;
-        int endMasha = bucketMasha + lengthMasha / 2;
+        int MatchLocation = Convert.ToInt32(Console.ReadLine());
+        int CountGoals1 = firstMatch1 + currentMatch1;
+        int CountGoals2 = firstMatch2 + currentMatch2;
 
-        int start = Math.Min(startVasya, startMasha);
-        int end = Math.Max(endVasya, endMasha);
-
-        if (endVasya < startMasha || endMasha < startVasya) 
+        if (CountGoals1 > CountGoals2)
         {
-            Console.WriteLine(lengthVasya + lengthMasha + 2); 
+            Console.WriteLine(0);
+            return;
+        }
+
+        if (CountGoals1 == CountGoals2 && MatchLocation == 2)
+        {
+            if (firstMatch1 <= currentMatch2)
+            {
+                Console.WriteLine(1);
+                return;
+            }
+            else
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+        }
+        else if (CountGoals1 == CountGoals2 && MatchLocation == 1)
+        {
+            if (currentMatch1 <= firstMatch2)
+            {
+                Console.WriteLine(1);
+                return;
+            }
+            else
+            {
+                Console.WriteLine(0);
+                return;
+            }
+        }
+
+        int goalsNeed = 0;
+        if (MatchLocation == 1)
+        {
+            goalsNeed = CountGoals2 - CountGoals1;
+            if (goalsNeed + currentMatch1 <= firstMatch2)
+            {
+                goalsNeed += 1;
+            }
         }
         else
         {
-            Console.WriteLine(end - start + 1);
+            goalsNeed = CountGoals2 - CountGoals1 + 1;
+            if (firstMatch1 > currentMatch2)
+            {
+                goalsNeed -= 1;
+            }
+
         }
+        Console.WriteLine(goalsNeed);
     }
 }
-
-
-// using System;
-// using System.Collections.Generic;
-
-// class Program
-// {
-//     static void Main()
-//     {
-//         string[] TreeVasya =Console.ReadLine().Split();
-//         string[] TreeMasha = Console.ReadLine().Split();
-//         int P = Convert.ToInt32(TreeVasya[0]);
-//         int V = Convert.ToInt32(TreeVasya[1]);
-//         int Q = Convert.ToInt32(TreeMasha[0]);
-//         int M = Convert.ToInt32(TreeMasha[1]);
-//         int min = Math.Min(P - V, Q - M);
-//         int max = Math.Max(P + V, Q + M);
-
-//         Dictionary <int,string> Trees = new Dictionary<int, string>();  
-//         for (int i = min; i <= max; i++){
-//             Trees.Add(i,"No");
-//         }
-//         for (int i = P - V; i <= P + V; i++){
-//             if (Trees[i] == "No"){
-//                 Trees[i] = "Yes";
-//             } else{
-//             continue;
-//             }
-//         }
-//         for (int i = Q - M; i <= Q + M; i++){
-//             if (Trees[i] == "No"){
-//                 Trees[i] = "Yes";
-//             } else{
-//             continue;
-//             }
-//         }
-//         int CountTrees = 0;
-//         foreach (var Tree in Trees){
-//             if (Tree.Value == "Yes"){
-//                  CountTrees++;
-//             } else {
-//                 continue;
-//             }
-//         } 
-//         Console.WriteLine(CountTrees);
-//     } 
-// }
