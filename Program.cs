0d8532b12814eb44087e80edcaf1a6c9cc9f1b14
Daniel <daniil.nyashka@gmail.com>
2024-03-03 16:42:50 +0300

    done

--- Program.cs
+++ Program.cs
@@ -4,70 +4,29 @@ class Program
 {
     static void Main()
     {
-        string[] firstMatch = Console.ReadLine().Split(':');
-        int firstMatch1 = Convert.ToInt32(firstMatch[0]);
-        int firstMatch2 = Convert.ToInt32(firstMatch[1]);
-
-        string[] currentMatch = Console.ReadLine().Split(':');
-        int currentMatch1 = Convert.ToInt32(currentMatch[0]);
-        int currentMatch2 = Convert.ToInt32(currentMatch[1]);
-
-        int MatchLocation = Convert.ToInt32(Console.ReadLine());
-        int CountGoals1 = firstMatch1 + currentMatch1;
-        int CountGoals2 = firstMatch2 + currentMatch2;
-
-        if (CountGoals1 > CountGoals2)
+        int n = Convert.ToInt32(Console.ReadLine());
+        double CountPress = 0;
+        for (int i = 0; i < n; i++)
         {
-            Console.WriteLine(0);
-            return;
+            double a = Convert.ToInt32(Console.ReadLine());
+            double remainder = a % 4;
+            decimal fraction = (decimal)a % 4 / 4M; 
+            switch (fraction)
+            {
+            case 0M:
+                    CountPress += a / 4;
+                break;
+            case 0.25M:
+                    CountPress += Math.Truncate( a/4 )+ 1;
+                break;
+            case 0.50M:
+                    CountPress += Math.Truncate( a/4 ) + 2;
+                break;
+            case 0.75M:
+                    CountPress += Math.Ceiling( a/4 )+ 1;
+                break;
+            }
+        }   
+        Console.WriteLine(CountPress); 
         }
-
-        if (CountGoals1 == CountGoals2 && MatchLocation == 2)
-        {
-            if (firstMatch1 <= currentMatch2)
-            {
-                Console.WriteLine(1);
-                return;
-            }
-            else
-            {
-                Console.WriteLine(0);
-                return;
-            }
-
-        }
-        else if (CountGoals1 == CountGoals2 && MatchLocation == 1)
-        {
-            if (currentMatch1 <= firstMatch2)
-            {
-                Console.WriteLine(1);
-                return;
-            }
-            else
-            {
-                Console.WriteLine(0);
-                return;
-            }
-        }
-
-        int goalsNeed = 0;
-        if (MatchLocation == 1)
-        {
-            goalsNeed = CountGoals2 - CountGoals1;
-            if (goalsNeed + currentMatch1 <= firstMatch2)
-            {
-                goalsNeed += 1;
-            }
-        }
-        else
-        {
-            goalsNeed = CountGoals2 - CountGoals1 + 1;
-            if (firstMatch1 > currentMatch2)
-            {
-                goalsNeed -= 1;
-            }
-
-        }
-        Console.WriteLine(goalsNeed);
     }
-}
