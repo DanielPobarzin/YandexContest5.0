682155a34c8401fb400b2f1a3ffcc2fb80037f33
Daniel <daniil.nyashka@gmail.com>
2024-03-06 09:03:30 +0300

    almost done

--- Program.cs
+++ Program.cs
@@ -1,153 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 
 class Program
 {
-     static void Main()
+    static void Main()
     {
-        int CeilCount;
-        int RookCount = 0;
-        int BishopCount = 0;
-        string[,] boardChess = new string[8, 8];
-        List<(int, int)> BPositions = new List<(int, int)>();
-        List<(int, int)> RPositions = new List<(int, int)>();
-        for (int i = 0; i < 8; i++)
-        {
-            string line = Console.ReadLine();
-            char[] item = line.ToCharArray();
-            for (int j = 0; j < 8; j++)
-            {
-                boardChess[i, j] = item[j].ToString();
-                switch(boardChess[i, j]){
-                case "B": BPositions.Add((i, j));
-                          BishopCount++;
-                          break;
-                case "R":
-                          RPositions.Add((i, j));
-                          RookCount++;
-                          break;
-                }
-            }
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] Line = Console.ReadLine().Split(' ');
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++){
+
+            numbers[i] = (Convert.ToInt64(Line[i]) % 2 == 0)? 2:1;
         }
-        CeilCount = 64 - (Bishop( BPositions, boardChess) + BishopCount) - (Rook(RPositions, boardChess) + RookCount);
-        Console.WriteLine(CeilCount);
+
+        List<string> Symbols = GenCombSymbols(numbers.Length - 1);
+
+         foreach (string combination in Symbols) 
+        {
+           string expression = GenExpression(combination, numbers);
+            DataTable equation = new DataTable();
+           int result = Convert.ToInt32(equation.Compute(expression, ""));
+                if (result % 2 != 0)
+                {
+                    Console.WriteLine(combination);
+                     break;
+                }
+        }
+    }   
+    static List<string> GenCombSymbols(int n)
+    {
+        List<string> Symbols = new List<string>();
+        GenerateComb(n, "", Symbols);
+        return Symbols;
     }
-        static int Bishop( List<(int, int)> BPositions, string[,] boardChess){
-            int Count = 0;
-        foreach (var position in BPositions){
-            int row = position.Item1;
-            int col = position.Item2;
 
-            for (int i = row + 1, j = col + 1; i < 8 && j < 8; i++, j++)
-            {
-                if (boardChess[i, j] == "*")
-                    {boardChess[i, j] = "x";
-                    Count++;
-                } else if (boardChess[i, j] == "x") {
-                        continue;
-                }else {
-                        break;
-                }
-            }
-            for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
-            {
-                if (boardChess[i, j] == "*")
-                    {boardChess[i, j] = "x";
-                    Count++;
-                }else if (boardChess[i, j] == "x") {
-                        continue;
-                }else {
-                        break;
-                }
-            }
-
-            for (int i = row - 1, j = col + 1; i >= 0 && j < 8; i--, j++)
-            {
-                if (boardChess[i, j] == "*")
-                {boardChess[i, j] = "x";
-                Count++;
-                }else if (boardChess[i, j] == "x") {
-                        continue;
-                }else {
-                        break;
-                }
-            }
-
-            for (int i = row + 1, j = col - 1; i < 8 && j >= 0; i++, j--)
-            {
-                if (boardChess[i, j] == "*")
-                {boardChess[i, j] = "x";
-                Count++;
-                }else if (boardChess[i, j] == "x") {
-                    continue;
-                }else {
-                    break;
-                }
-            }
-         }
-         return Count;
-        }
-         static int Rook( List<(int, int)> RPositions, string[,] boardChess){
-          int Count = 0;
-          foreach (var position in RPositions)
+    static void GenerateComb(int n, string nowSymbols, List<string> Symbols)
+    {
+        if (n == 0)
         {
-            
-            int row = position.Item1;
-            int col = position.Item2;
+            Symbols.Add(nowSymbols);
+            return;
+        }
 
-            for (int j = col - 1; j >= 0; j--)
+        GenerateComb(n - 1, nowSymbols + "x", Symbols);
+        GenerateComb(n - 1, nowSymbols + "+", Symbols);
+    }
+
+     static string GenExpression(string combination , int[] numbers)
+    {
+         string expression = numbers[0].ToString();
+            for (int i = 0; i < combination.Length; i++)
             {
-                if (boardChess[row, j] == "*")
-                {boardChess[row, j] = "x";
-                    Count++;
+                if (combination[i] == 'x')
+                {
+                    expression += " * ";
                 }
-                else if (boardChess[row, j] == "x") {
-                    continue;
-                }else {
-                    break;
+                else if (combination[i] == '+')
+                {
+                    expression += " + ";
                 }
+                expression = expression + numbers[i + 1].ToString();
             }
-
-            for (int j = col + 1; j < 8; j++)
-            {
-                if (boardChess[row, j] == "*")
-                { boardChess[row, j] = "x";
-                    Count++;
-                }
-                else if (boardChess[row, j] == "x") {
-                    continue;
-                }else {
-                    break;
-                }
-            }
-
-            for (int i = row - 1; i >= 0; i--)
-            {
-                if (boardChess[i, col] == "*")
-                {boardChess[i, col] = "x";
-                    Count++;
-                }
-               else if (boardChess[i, col] == "x") {
-                    continue;
-                }else {
-                    break;
-                }
-            }
-
-            for (int i = row + 1; i < 8; i++)
-            {
-                if (boardChess[i, col] == "*")
-                {boardChess[i, col] = "x";
-                    Count++;
-                }
-                      else if (boardChess[i, col] == "x") {
-                    continue;
-                }else {
-                    break;
-                }
-            }
-       }
-       return Count;
-       }
-}
-
+            return expression;
+    }
+       
+} 
